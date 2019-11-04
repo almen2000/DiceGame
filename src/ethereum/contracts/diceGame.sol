68ed9ff710394944aa48626c86f4b201764bc3ac
Alexandr <alexandr@localhost.localdomain>
2019-11-04 18:36:28 +0400

    changes

--- src/ethereum/contracts/diceGame.sol
+++ src/ethereum/contracts/diceGame.sol
@@ -5,18 +5,39 @@ contract DiceGame {
     uint public gameId;
     uint public nextGameMinimumBet;
     uint public nextGameMaximumBet;
+    uint public prevGameStartTime;
     uint private canBeTransferredToServerMoney;
 
+    struct Player {
+        bytes32 userHash;
+        uint betValue;
+        bool received;
+    }
 
+    struct Game {
+        mapping(address => Player) players;
+        uint8 dice1;
+        uint8 dice2;
+        uint gameBalance;
+        uint minimumBet;
+    }
+
+    mapping(uint => Game) games;
 
     modifier isManager() {
         require(msg.sender == manager, "Only manager can call this function");
         _;
     }
 
-    constructor(uint _minimumBet) public {
+    modifier between1and6(uint8 dice1, uint8 dice2) {
+        require(dice1 >= 1 && dice1 <= 6 && dice2 >= 1 && dice2 <= 6, "One of the dices are not between 1 and 6");
+        _;
+    }
+
+    constructor(uint _minimumBet, uint _maximumBet) public {
         manager = msg.sender;
         _minimumBet = 1;
+        _maximumBet = 1;
         gameId = 1;
     }
 
@@ -29,8 +50,24 @@ contract DiceGame {
         msg.sender.transfer(canBeTransferredToServerMoney);
     }
 
-    function signData(bytes memory dataToSign) pure public returns (bytes32) {
-        return sha256(dataToSign);
+    function hash(uint8 dice1, uint8 dice2, string memory password) public pure between1and6(dice1, dice2) returns (bytes32) {
+        return keccak256('7');
     }
+
+    function hashUserBet(uint8 dice1, uint8 dice2, string memory password) private pure returns (bytes32) {
+        bytes memory _password = bytes(password);
+        uint length = _password.length;
+        bytes memory strToHash = new bytes(length + 2);
+        for (uint8 _i = 0; _i < length; _i++) strToHash[_i] = _password[_i];
+        strToHash[length] = byte(48 + dice1);
+        strToHash[length + 1] = byte(48 + dice2);
+        return keccak256(strToHash);
+    }
+
+    function f() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function transfer() public payable {}
 
 }
